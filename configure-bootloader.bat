a9ef0e9a5444214a52959434220a03024d14b35b
albertanguyen <jaynguyen2@outlook.com>
2020-07-07 00:25:47 -0500

    Add instructing comments

--- configure-bootloader.bat
+++ configure-bootloader.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 :: This script needs to be excuted with administration privilige
+:: UEFI BIOS mode: set rEFInd as default boot loader
 ECHO ============================
 ECHO SET rEFInd AS DEFAULT BOOT LOADER
 ECHO ============================
@@ -12,8 +13,13 @@ cd C:\Users\HP\Downloads\refind-bin-0.12.0 && dir
 xcopy /E refind S:\EFI\refind\
 cd /d S:\EFI\refind
 ECHO REMOVE UNNECESSARY DRIVERS
+:: Legacy BIOS mode: set ??? as default boot loader
+:: x64-based PC
+:: TODO: get value of System Type and compare with "x64-based PC"
 RMDIR /S /Q drivers_aa64 && del  refind_aa64.efi && RMDIR /S /Q tools_aa64
 RMDIR /S /Q drivers_ia32 && del refind_ia32.efi && RMDIR /S /Q tools_ia32
+
+:: x86-based PC
 rename refind.conf-sample refind.conf
 bcdedit /set "{bootmgr}" path \EFI\refind\refind_x64.efi
 bcdedit /set "{bootmgr}" description "rEFInd as the default EFI boot program"
