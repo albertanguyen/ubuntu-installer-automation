44c2c686ffe63e89e8967caa343dd273ea83c21a
albertanguyen <jaynguyen2@outlook.com>
2020-07-07 23:01:52 -0500

    retrieve OS info to be used later in other scripts

--- pre-installation.bat
+++ pre-installation.bat
@@ -1,11 +1,27 @@
 @ECHO OFF
 :: This script needs to be excuted with administration privilige
+REM GOTO :hardwareInfo
+:osInfo
 ECHO ============================
 ECHO OS INFO
 ECHO ============================
-systeminfo | findstr /c:"OS Name"
-systeminfo | findstr /c:"OS Version"
-systeminfo | findstr /c:"System Type"
+FOR /F "tokens=*" %%i IN ('systeminfo ^| findstr /c:"OS Name"') do (
+    SET _os_name=%%i
+    ECHO %_os_name%
+)
+:: systeminfo | findstr /c:"OS Version"
+FOR /F "tokens=*" %%i IN ('systeminfo ^| findstr /c:"BIOS Version"') do (
+    SET _bios_ver=%%i
+    ECHO %_bios_ver%
+)
+
+FOR /F "tokens=*" %%i IN ('systeminfo ^| findstr /c:"System Type"') do (
+    SET _system_type=%%i
+    ECHO %_system_type%
+)
+GOTO :eof
+
+:hardwareInfo
 ECHO ============================
 ECHO HARDWARE INFO
 ECHO ============================
@@ -16,7 +32,8 @@ systeminfo | findstr /c:"Virtual Memory: Max Size"
 systeminfo | findstr /c:"Boot Device"
 systeminfo | findstr /c:"Total Physical Memory"
 systeminfo | findstr /c:"Virtual Memory: Max Size"
-:: This step is used to check if the disk contains single partition or multiple partitions
+
+:diskInfo
 ECHO ============================
 ECHO DISK PARTITION INFO
 ECHO ============================
