94c1c13251ff1c552352c2f5822172410cfb4711
albertanguyen <jaynguyen2@outlook.com>
2020-07-08 12:06:17 -0500

    separate cleanup step for x64-based PC

--- bootloader-cleanup.bat
+++ bootloader-cleanup.bat
@@ -1,12 +1,22 @@
 @ECHO OFF
 ECHO bat file worked!
 :: This script needs to be excuted with administration privilige
-ECHO ============================
-ECHO CLEANUP OF EFI BOOT LOADER
-ECHO Restoring UEFI boot entry in BCD store
-ECHO ============================
-bcdedit /set "{bootmgr}" path \EFI\Boot\bootx64.efi
-bcdedit /set "{bootmgr}" description "default UEFI boot"
+
+IF /I "%systype%"=="x86-based PC" (
+    ECHO configure boot manager for x86 Arch
+) ELSE (
+    GOTO :commonExit
+)
+IF /I "%systype%"=="x64-based PC" (
+    ECHO ============================
+    ECHO CLEANUP OF EFI BOOT LOADER
+    ECHO Restoring UEFI boot entry in BCD store
+    ECHO ============================
+    bcdedit /set "{bootmgr}" path \EFI\Boot\bootx64.efi
+    bcdedit /set "{bootmgr}" description "default UEFI boot"
+) ELSE (
+    GOTO :commonExit
+)
 ECHO ============================
 ECHO Unmount ESP
 ECHO ============================
@@ -19,3 +29,6 @@ ECHO Enable the hibernate feature
 ECHO ============================
 powercfg /h on
 reg query "HKLM\SYSTEM\CurrentControlSet\Control\Power" /v "HibernateEnabled"
+
+:commonExit
+EXIT /b 1
